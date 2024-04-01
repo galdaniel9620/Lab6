ffa8a60bc93071a78899acf690724ed0577a3faf
Daniel Gal <47724705+galdaniel9620@users.noreply.github.com>
2024-04-01 23:40:04 +0300

    Add project files.

---
+++ Lab6ex1/Program.cs
@@ -0,0 +1,46 @@
+﻿//Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
+//metoda care va calcula volumul acestuia.
+//Folositi o clasa care va modela un dulap.
+// Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+// Cele trei campuri vor fi initializate cu ajutorul constructorului
+// Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+//Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
+//afisati rezultatul.
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter length of the wardrobe:");
+        int length = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter width of the wardrobe:");
+        int width = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter height of the wardrobe:");
+        int height = int.Parse(Console.ReadLine());
+
+        Wardrobe myCupboard = new Wardrobe(length, width, height);
+        int volume = myCupboard.CalculateVolume();
+        Console.WriteLine("The volume of the wardrobe is: " + volume);
+    }
+}
+
+public class Wardrobe
+{
+    private int length;
+    private int width;
+    private int height;
+
+    public Wardrobe(int length, int width, int height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int CalculateVolume()
+    {
+        return length * width * height;
+    }
+}
---
+++ Lab6ex2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Scrieti un program care va modela un autoturism. Un autoturism va avea o
+//marca (string), un numar de inmatriculare (string), precum si o
+//capacitate cilindrica(int).
+//Autoturismul va avea:
+// Campurile corespunzatoare caracteristicilor autoturismului.
+// Un constructor care va initializa campurile acestuia
+// O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma : “marca,
+//numarDeInmatriculare, capacitateCilindrica”.
+//Folositi: clase, campuri, metode, modificatorii de acces adecvati, comentarii
+//xml pentru metodele publice.
+//Creeati doua obiecte de tipul autoturism in functia “Main” si apelati-le
+//metodele “Tipareste
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter car details:");
+
+        Console.Write("Enter car brand: ");
+        string brand = Console.ReadLine();
+
+        Console.Write("Enter car registration number: ");
+        string registrationNumber = Console.ReadLine();
+
+        Console.Write("Enter car engine capacity: ");
+        int engineCapacity = int.Parse(Console.ReadLine());
+
+        Car car = new Car(brand, registrationNumber, engineCapacity);
+
+        Console.WriteLine("\nCar details:");
+        car.Print();
+    }
+}
+
+public class Car
+{
+    private string brand;
+    private string registrationNumber;
+    private int engineCapacity;
+
+    public Car(string brand, string registrationNumber, int engineCapacity)
+    {
+        this.brand = brand;
+        this.registrationNumber = registrationNumber;
+        this.engineCapacity = engineCapacity;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"{brand}, {registrationNumber}, {engineCapacity}cc");
+    }
+}
+
